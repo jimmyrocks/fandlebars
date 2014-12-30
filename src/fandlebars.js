e3a6dfceffce1fceb8d8614ed263f9058e3c047d
Jim McAndrew <jim@loc8.us>
2014-12-30 16:21:10 -0700

    Add source code

---
+++ src/fandlebars.js
@@ -0,0 +1,50 @@
+module.exports = function(text, origTree) {
+  // This is my quick and dirty version of handlebars
+  var re = function(name) {
+      return new RegExp('{{' + name + '}}', 'g');
+    },
+    replaceables,
+    replaceAddress,
+    replaceValueId,
+    treeSearch = function(addresses, tree) {
+      var tasks = {
+          'object': function(a, t) {
+            // Recursively search through the object
+            return treeSearch(a.slice(1), t[a[0]]);
+          },
+          'array': function(a, t) {
+            if (a.length > 1 && t[a[0]]  && typeof parseInt(a[1], 10) === 'number') {
+              return treeSearch(a.slice(1), t[a[0]]);
+            } else if (a.length === 1) {
+              return t[a[0]];
+            }
+          },
+          'function': function(a, t) {
+            var returnValue;
+            returnValue = t[a[0]](a.slice(1), t);
+            return a.length > 2 ? treeSearch(a.slice(2), returnValue) : returnValue;
+          },
+          'default': function(a, t) {
+            return a.length === 1 ? t[a[0]] : undefined;
+          }
+        },
+        taskManager = function(a, t) {
+          var type = Object.prototype.toString.call(t[a[0]]).replace(/\[object (.+?)\]/, '$1').toLowerCase(),
+            fn = tasks[type] ? tasks[type] : tasks['default'];
+          return fn(a, t);
+        };
+      return taskManager(addresses, tree);
+    };
+
+  if (typeof origTree === 'object') {
+    replaceables = text.match(re('.+?'));
+    if (replaceables) {
+      for (replaceValueId = 0; replaceValueId < replaceables.length; replaceValueId++) {
+        replaceAddress = replaceables[replaceValueId].replace(re('(.+?)'), '$1').split('.');
+        text = text.replace(replaceables[replaceValueId], treeSearch(replaceAddress, origTree));
+      }
+    }
+  }
+
+  return text;
+};
