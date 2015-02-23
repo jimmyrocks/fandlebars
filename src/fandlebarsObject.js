3b5a2bb93e55e87af801cf7f14ec7ecee79e9663
Jim McAndrew <jim@loc8.us>
2015-02-23 16:12:31 -0700

    Only fandlebars strings

--- src/fandlebarsObject.js
+++ src/fandlebarsObject.js
@@ -6,8 +6,10 @@ var fbobj = function(obj, tree) {
   for (var v in obj) {
     if (typeof obj[v] === 'object') {
       newObj[v] = fbobj(obj[v], tree);
-    } else {
+    } else if (typeof obj[v] === 'string') {
       newObj[v] = fandlebars(obj[v], tree);
+    } else {
+      newObj[v] = obj[v];
     }
   }
   return newObj;
