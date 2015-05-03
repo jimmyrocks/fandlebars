f403e352e4929a412cb99273c570b82c64aed35b
Jim McAndrew <jim@loc8.us>
2015-05-03 17:38:19 -0600

    Add the processing parameter

--- src/fandlebarsObject.js
+++ src/fandlebarsObject.js
@@ -1,13 +1,13 @@
 //Updates an entire object using fandlebars
 var fandlebars = require('./fandlebars.js');
 
-var fbobj = function(obj, tree) {
+var fbobj = function(obj, tree, processing) {
   var newObj = Array.isArray(obj) ? [] : {};
   for (var v in obj) {
     if (typeof obj[v] === 'object') {
       newObj[v] = fbobj(obj[v], tree);
     } else if (typeof obj[v] === 'string') {
-      newObj[v] = fandlebars(obj[v], tree);
+      newObj[v] = fandlebars(obj[v], tree, processing);
     } else {
       newObj[v] = obj[v];
     }
